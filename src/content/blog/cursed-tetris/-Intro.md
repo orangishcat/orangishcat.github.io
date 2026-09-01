f88c3a58d0fc145d21f3cf6f12c2c7e45770fb33
orangishcat <fdsnail2@gmail.com>
2026-08-31 23:04:48 -0700

    blog: cursed-tetris blog post

---
+++ src/content/blog/cursed-tetris/-Intro.md
@@ -0,0 +1,25 @@
+---
+title: "Intro"
+---
+
+Hi! I made tetris with powerups that runs fully in your terminal. To try it out, just generate an SSH key `ssh-keygen` if you don't already have one, then `ssh` into the server. Your SSH key is your identity; no account is required.
+
+```
+ssh play@cursedtetris.orangishcat.dev
+```
+
+If you want to play offline, you can also [download from GitHub Releases](https://github.com/orangishcat/cursed-tetris/releases/latest).
+
+It is recommended to use a [Nerd Font](https://www.nerdfonts.com/) so that all text renders correctly.
+
+Star my [repo](https://github.com/orangishcat/cursed-tetris) if you liked the game :D
+
+---
+
+My friends challenged me to make a "text-based game". I assume they meant an RPG, but surely a working tetris game in the terminal counts as text-based, right? It's drawing text, except some of that text happens to be full unicode characters and look like tetris pieces.
+
+I have no idea why I chose tetris as the game I want to make. I barely even know how to play!
+
+Besides, this is a great excuse to learn Rust. I've always been a learning by doing person, and just reading about ownership and borrowing doesn't really give me a great understanding of it. Actually making a game with it would help significantly.
+
+As with any project, even a "simple" game ended up being quite the challenge. Next are my devlogs where I wrote one pretty much every day I was working on the project until its eventual completion. Hope you enjoy!
--- src/data.ts
+++ src/data.ts
@@ -6,42 +6,58 @@ export const projects = [
   },
   {
     title: "Better Clouds",
-    description: "A Minecraft mod that adds volumetric clouds in touch with the aesthetic of vanilla Minecraft",
+    description:
+      "A Minecraft mod that adds volumetric clouds in touch with the aesthetic of vanilla Minecraft",
     url: "https://modrinth.com/mod/better-clouds",
-    note: "Maintainer"
-  },
-  {
-    title: "Page Proxy",
-    description: "A Chrome/Firefox extension that streamlines the process of creating userscripts.",
-    url: "https://orangishcat.github.io/page-proxy/"
+    note: "Maintainer",
   },
   {
     title: "cursed-tetris",
-    description: "Tetris with powerups, fully in your terminal. My first ever Rust project! I quite enjoyed learning Rust.",
-    url: "https://github.com/orangishcat/cursed-tetris"
+    description:
+      "Tetris with powerups, fully in your terminal. My first ever Rust project! I quite enjoyed learning Rust.",
+    url: "https://github.com/orangishcat/cursed-tetris",
+  },
+  {
+    title: "Page Proxy",
+    description:
+      "A Chrome/Firefox extension that streamlines the process of creating userscripts.",
+    url: "https://orangishcat.github.io/page-proxy/",
   },
   {
     title: "Tanks: the Crusades",
-    description: "A cross-platform tank game where the player controls a blue tank with the goal of destroying all enemy tanks to clear a level.",
+    description:
+      "A cross-platform tank game where the player controls a blue tank with the goal of destroying all enemy tanks to clear a level.",
     url: "https://github.com/aehmttw/Tanks",
-    note: "Contributor"
+    note: "Contributor",
   },
 ];
 
 export const blogs = [
   {
+    slug: "cursed-tetris",
+    title: "I made a terminal-based tetris game as my first Rust project!",
+    description:
+      "After learning it, I can say that Rust is a pretty cool language. I've also gotten kinda addicted to my own game.",
+    date: "August 31, 2026",
+  },
+  {
     slug: "personal-website",
     title: "I learned art, design, and webdev to create my personal website!!",
-    description: "One of the most rewarding projects I have ever done. I learned so much! It is also the only project of mine as of right now that is mostly finished.",
-    date: "July 14, 2026"
+    description:
+      "One of the most rewarding projects I have ever done. I learned so much! It is also the only project of mine as of right now that is mostly finished.",
+    date: "July 14, 2026",
   },
   {
     slug: "page-proxy",
     title: "Page Proxy Devlogs",
-    description: "Devlogs about my Chrome/Firefox extension and userscript manager, Page Proxy!",
-    date: "April 30, 2026"
-  }
-]
+    description:
+      "Devlogs about my Chrome/Firefox extension and userscript manager, Page Proxy!",
+    date: "April 30, 2026",
+  },
+];
 
-export const routeFor = (entry: any, index: number, prefix: string = "/blog/") =>
-  prefix + (index === 0 ? entry.id.split("/")[0] : entry.id);
+export const routeFor = (
+  entry: any,
+  index: number,
+  prefix: string = "/blog/",
+) => prefix + (index === 0 ? entry.id.split("/")[0] : entry.id);
